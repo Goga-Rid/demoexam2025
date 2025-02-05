d79db6d75ec3bdbbd700ff03dda7de26e298f3ba
Goga-Rid <wopa22@list.ru>
2025-02-05 16:35:44 +0300

    [FIX] readme fix

--- src/main/java/com/example/demoexam2025/Partner.java
+++ src/main/java/com/example/demoexam2025/Partner.java
@@ -16,6 +16,18 @@ public class Partner {
     private String legal_address;
     private String inn;
     private int rating;
-    private int totalSales; ;
+    private int totalSales;
+
+    public Partner(int partnerId, String partnerType, String partnerName, String director, String email, String phone, String legalAddress, String inn, int rating) {
+        this.partnerId = partnerId;
+        this.partner_type = partnerType;
+        this.partner_name = partnerName;
+        this.director = director;
+        this.email = email;
+        this.phone = phone;
+        this.legal_address = legalAddress;
+        this.inn = inn;
+        this.rating = rating;
+    }
 }
 
